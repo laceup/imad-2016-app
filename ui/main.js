d0c072f21de2405a090fd214b4c083c804e55c6c
Aparna Mohan <laceup@users.noreply.github.com>
2016-11-26 23:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 console.log('Loaded!');
 alert('halllo');
-$(document).ready(function(){
+document.onreadystatechange = function(){
     var button = document.getElementById('bu');
 var c = 0;
 button.onclick=function(){
     c=c+1;
     var span=document.getElementById('span1');
     span.InnerHtml=c.toString();
-    };
-});
+    
+};
+};
 
